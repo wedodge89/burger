f61af73aa793922b779811ad15e6429ae56c217c
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-01-04 11:06:01 -0500

    wrote burger.js file to use orm w/ burgers table

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,23 @@
 const orm = require("./../config/orm");
+
+var burgers = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burgers;
