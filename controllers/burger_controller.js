849123ce2bca6870b6e1b1c1828ae77a67cf53ef
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-01-04 14:44:32 -0500

    added burger controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,9 +3,40 @@ const router = express.Router();
 
 const burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId});
+    });
+});
 
+router.put("/api/burges/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
 
+    console.log("condition", condition);
 
+    burger.update ({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
