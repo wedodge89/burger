6a71c099323d01fdf5bd21e3e0681b310eb1566a
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-01-07 18:40:16 -0500

    worked on display/handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "BurgerPass",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "BurgerPass",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
